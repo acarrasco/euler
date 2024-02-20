8b0a405f26685c1b2ca9e5a2ab9022fc571a862c
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2024-02-20 16:09:55 +0100

    bugfixes and tests for euler_common.py
    For some reason I was using `is` instead of `==` to compare numbers
    in `is_prime` which caused every prime larger than 256 to test as
    non-prime. Interning is fun!
    
    Also the `gcd` function didn't have a correct implementation of Euler's
    algorithm, it stopped one step too early.

--- python/euler_common.py
+++ python/euler_common.py
@@ -5,34 +5,67 @@ import collections
 
 _primes = list(map(int, open("10000primes.txt").read().split()))
 
-def _grow_primes():
+def _grow_primes(_primes=_primes):
+    '''
+    >>> primes = [2, 3]
+    >>> _grow_primes(primes)
+    >>> len(primes)
+    102
+    >>> len({2, 3, 5, 7, 11, 13} & set(primes))
+    6
+    >>> len({4, 6, 8, 9, 10, 12, 14, 15, 16, 18} & set(primes))
+    0
+    '''
+    _primes 
     n = _primes[-1] + 2
     for _ in range(100):
         while not all(n % p for p in _primes):
             n += 2
         _primes.append(n)
 
-def is_prime(n):
+def is_prime(n, _primes=_primes):
+    '''
+    >>> is_prime(1)
+    False
+    >>> is_prime(257)
+    True
+    >>> is_prime(109453)
+    True
+    >>> is_prime(109455)
+    False
+    >>> is_prime(109459)
+    False
+    '''
     if _primes[-1] < n:
         next_prime(n)
     i = bisect.bisect_left(_primes, n)
-    return _primes[i] is n
+    return _primes[i] == n
 
-def next_prime(n):
-    i = bisect.bisect_left(_primes, n+1)
-    if i < len(_primes):
-        return _primes[i]
-    while _primes[-1] <= n:
-        _grow_primes()
-    return _primes[bisect.bisect(_primes, n)]
+def next_prime(n, primes=_primes):
+    i = bisect.bisect_left(primes, n+1)
+    if i < len(primes):
+        return primes[i]
+    while primes[-1] <= n:
+        _grow_primes(primes)
+    return primes[bisect.bisect(primes, n)]
 
-def primes():
+def primes(_primes=_primes):
+    '''
+    >>> list(zip(range(10), primes()))
+    [(0, 2), (1, 3), (2, 5), (3, 7), (4, 11), (5, 13), (6, 17), (7, 19), (8, 23), (9, 29)]
+    '''
     p = 1
     while True:
-        p = next_prime(p)
+        p = next_prime(p, _primes)
         yield p
 
-def nth_prime(n):
+def nth_prime(n, _primes=_primes):
+    '''
+    >>> nth_prime(1)
+    2
+    >>> nth_prime(10)
+    29
+    '''
     while n > len(_primes):
         _grow_primes()
     return _primes[n-1]
@@ -66,8 +99,16 @@ def solve_diophantine(d):
         if x*x - d*y*y == 1:
             return x, y
 
-def factors(n):
-    for p in primes():
+def factors(n, _primes=_primes):
+    '''
+    >>> list(factors(1))
+    []
+    >>> list(factors(6))
+    [2, 3]
+    >>> list(factors(8))
+    [2, 2, 2]
+    '''
+    for p in primes(_primes):
         while n % p == 0 and n > 1:
             yield p
             n /= p
@@ -75,19 +116,45 @@ def factors(n):
             return
 
 def prod(*args):
+    '''
+    >>> prod()
+    1
+    >>> prod(2, 3, 4)
+    24
+    '''
     acc = 1
     for i in args:
         acc *= i
     return acc
 
 def gcd(n, m):
+    '''
+    >>> gcd(2, 3)
+    1
+    >>> gcd(3, 2)
+    1
+    >>> gcd(4, 2)
+    2
+    >>> gcd(2**10, 256)
+    256
+    '''
     if n < m:
         n, m = m, n
-    while m > 1:
+    while m > 0:
         n, m = m, n % m
     return n
 
 def mcm(*args):
+    '''
+    >>> mcm(2, 3, 5)
+    30
+    >>> mcm(2, 2, 4)
+    4
+    >>> mcm(1, 2, 2, 4)
+    4
+    >>> mcm(1, 2, 2, 4, 10)
+    20
+    '''
     deduped_factors = collections.defaultdict(int)
     for n in args:
         grouped_factors = collections.Counter(factors(n))
@@ -96,4 +163,9 @@ def mcm(*args):
     return prod(*[f ** p for (f, p) in deduped_factors.items()])
 
 def by_tuples(lst, size):
+    '''
+    # staggered tuples of `size` elements
+    >>> list(by_tuples(range(6), 3))
+    [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5)]
+    '''
     return zip(*[lst[i:] for i in range(size)])
--- python/fast_primes.py
+++ python/fast_primes.py
@@ -3,7 +3,7 @@ import numpy
 def primesfrom2to(n):
     """ Input n>=6, Returns a array of primes, 2 <= p < n """
     sieve = numpy.ones(n/3 + (n%6==2), dtype=numpy.bool)
-    for i in xrange(1,int(n**0.5)/3+1):
+    for i in range(1,int(n**0.5)/3+1):
         if sieve[i]:
             k=3*i+1|1
             sieve[       k*k/3     ::2*k] = False
@@ -15,10 +15,10 @@ def primesfrom2to(n):
 def primes1(n):
     """ Returns  a list of primes < n """
     sieve = [True] * (n/2)
-    for i in xrange(3,int(n**0.5)+1,2):
+    for i in range(3,int(n**0.5)+1,2):
         if sieve[i/2]:
             sieve[i*i/2::i] = [False] * ((n-i*i-1)/(2*i)+1)
-    return [2] + [2*i+1 for i in xrange(1,n/2) if sieve[i]]
+    return [2] + [2*i+1 for i in range(1,n/2) if sieve[i]]
 
 def primes2(n):
     """ Input n>=6, Returns a list of primes, 2 <= p < n """
